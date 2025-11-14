582f48efe49571f38a668da545d6009daad53582
Jingwei He <jingweihe198@gmail.com>
2025-11-13 16:58:31 -0800

    start

---
+++ src/main/java/PatientTriage/PatientTriageApplication.java
@@ -0,0 +1,13 @@
+package PatientTriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientTriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PatientTriage/PatientTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package PatientTriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
